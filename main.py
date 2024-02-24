a0bb2f5e655d27a49f06a72900dc34b2dfde15f9
Emile Deloof <deloofemile@gmail.com>
2024-02-24 12:05:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import RPLCD
+import smbus2
+
+url = "http://127.0.0.1:5001"
+
+print("What would you like to test? \n1) Reset DB \n2) Add goal \n3) Add attempt")
+input = input()
+input = int(input)
+if input == 1:
+    request = requests.post(url+"/requests/reset", data = json.dumps({"message": "hello there"}), headers={"Content-Type": "Application/Json"})
+    print(request.text)
+elif input == 2:
+    request = requests.post(url+"/requests/goal")
+    print(request)
+
