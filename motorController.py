89f88c9ab3b330cef0f2342dc36254df4041c293
Emile Deloof <deloofemile@gmail.com>
2024-03-05 19:54:21 +0100

    cleaning up

--- motorController.py
+++ motorController.py
@@ -1,50 +1,25 @@
-# Based on: https://www.raspberrypi.org/forums/viewtopic.php?t=242928\.
-#
-# Software to drive 4 wire stepper motor using a TB6600 Driver
-# PRi - RPi 3B
-#
-# Route 3.3 VDC to the controller "+" input for each: ENA, PUL, and DIR
-#
-# Connect GPIO pins as shown below) to the "-" input for each: ENA, PUL, and DIR
-#
-#
 from time import sleep
 import RPi.GPIO as GPIO
-#
+
 PUL = 14  # Stepper Drive Pulses
 DIR = 15  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
 ENA = 18  # Controller Enable Bit (High to Enable / LOW to Disable).
-# DIRI = 14  # Status Indicator LED - Direction
-# ENAI = 15  # Status indicator LED - Controller Enable
-#
-# NOTE: Leave DIR and ENA disconnected, and the controller WILL drive the motor in Default direction if PUL is applied.
-# 
+
 GPIO.setmode(GPIO.BCM)
-# GPIO.setmode(GPIO.BOARD) # Do NOT use GPIO.BOARD mode. Here for comparison only. 
-#
+
 GPIO.setup(PUL, GPIO.OUT)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(ENA, GPIO.OUT)
-# GPIO.setup(DIRI, GPIO.OUT)
-# GPIO.setup(ENAI, GPIO.OUT)
-#
-# print('PUL = GPIO ' + PUL + ' - RPi 3B-Pin #8')
-# print('DIR = GPIO ' + DIR + ' - RPi 3B-Pin #10')
-# print('ENA = GPIO ' + ENA + ' - RPi 3B-Pin #12')
-# print('ENAI = GPIO 14 - RPi 3B-Pin #8')
-# print('DIRI = GPIO 15 - RPi 3B-Pin #10')
 
-#
 print('Initialization Completed')
-#
-# Could have usesd only one DURATION constant but chose two. This gives play options.
+
 durationFwd = 5000 # This is the duration of the motor spinning. used for forward direction
 durationBwd = 5000 # This is the duration of the motor spinning. used for reverse direction
 print('Duration Fwd set to ' + str(durationFwd))
 print('Duration Bwd set to ' + str(durationBwd))
-#
+
 # delay = 0.0000001 # This is actualy a delay between PUL pulses - effectively sets the mtor rotation speed.
-delay = 0.0000001
+delay = 0.0000000001
 print('Speed set to ' + str(delay))
 #
 cycles = 10 # This is the number of cycles to be run once program is started.
