5a29a70ff8a1ab9a66221673a596983a7d9a90a7
Emile Deloof <deloofemile@gmail.com>
2024-03-07 17:17:33 +0100

    changed ultrasone

--- ultrasoon.py
+++ ultrasoon.py
@@ -1,57 +1,34 @@
-# Import required Python libraries
-import time
 import RPi.GPIO as GPIO
+import time
 
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+try:
+     GPIO.setmode(GPIO.BOARD)
+     PIN_TRIGGER = 7
+     PIN_ECHO = 11
 
-# Define GPIO to use on Pi
-GPIO_TRIGGER = 23
-GPIO_ECHO    = 24
+     GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+     GPIO.setup(PIN_ECHO, GPIO.IN)
+     GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
+     print("Setting up Sensor")
 
+     time.sleep(2)
 
-# Set pins as output and input
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+     while(True):
+         GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+         time.sleep(0.00001)
+         GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
+         while GPIO.input(PIN_ECHO)==0:
+               pulse_start_time = time.time()
 
-print("Ultrasonic Measurement")
+         while GPIO.input(PIN_ECHO)==1:
+               pulse_end_time = time.time()
 
-# Allow module to settle
-time.sleep(0.5)
+         pulse_duration = pulse_end_time - pulse_start_time
+         distance = round(pulse_duration * 17150, 2)
+         print("Distance:",distance,"cm")
+         time.sleep(0.1)
 
-# Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
-
-while GPIO.input(GPIO_ECHO)==0:
-  start = time.time()
-
-while GPIO.input(GPIO_ECHO)==1:
-  stop = time.time()
-
-# Calculate pulse length
-elapsed = stop-start
-
-# Distance pulse travelled in that time is time
-# multiplied by the speed of sound (cm/s)
-distancet = elapsed * 34300
-
-# That was the distance there and back so halve the value
-distance = distancet / 2
-
-print("Distance :", distance)
-
-print ("Elaspsed time :", elapsed)
-
-print ("Total distance :", distancet)
-
-# Reset GPIO settings
-GPIO.cleanup()
+finally:
+     GPIO.cleanup()
