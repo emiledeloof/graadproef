a6fe64d640427de6bc33806626492e42708a5a7d
Emile Deloof <deloofemile@gmail.com>
2024-03-20 21:27:58 +0100

    removed whitespace

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ parsedGoals = json.loads(goals.text)
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dotsize=8)
 lcd.clear()
 
-lcd.write_string('Attempts: ' + str(parsedAttempts['attempts']) + "\n\rGoals: " + str(parsedGoals['goals']) + "    => " + str(parsedAttempts['attempts'] / parsedGoals['goals']))
+lcd.write_string('Attempts: ' + str(parsedAttempts['attempts']) + "\n\rGoals: " + str(parsedGoals['goals']) + " => " + str(parsedAttempts['attempts'] / parsedGoals['goals']))
